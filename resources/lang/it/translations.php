a9841f4db7ed8eb00bc7ab7918f658468e5893b1
Mario Gattolla <94918437+MarioGattolla@users.noreply.github.com>
2026-09-11 10:04:52 +0200

    feat: support for Italian language (#54)

---
+++ resources/lang/it/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Titolo',
+    'description' => 'Descrizione',
+    'author' => 'Autore',
+    'image' => 'Immagine',
+    'characters' => 'Caratteri',
+    'robots' => 'Meta Tag Robots',
+];
